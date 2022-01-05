2bb97dff57895bde63baa47fad62f9d2982faf94
Matheus Alves <malves224@gmail.com>
2022-01-05 20:04:46 -0300

    adiciona query para o desafio3

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,5 @@
 SELECT 
 COUNT(DISTINCT(m.id_musica)) as cancoes, 
 COUNT(DISTINCT(ar.id_artista)) as artistas, 
-COUNT(DISTINCT(ab.id_album)) 
+COUNT(DISTINCT(ab.id_album)) as albuns
 FROM musicas as m, album as ab, artistas as ar;
