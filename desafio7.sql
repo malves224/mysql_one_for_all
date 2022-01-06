9554ff8048b19d7f2494ea5b312ee295a77c5395
Matheus Alves <malves224@gmail.com>
2022-01-06 20:37:58 -0300

    cria querry para o desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,8 @@
+SELECT a.nome AS artista, ar.nome AS album, count(ua.id_artista) AS seguidores  FROM artistas AS a
+INNER JOIN album AS ar
+ON a.id_artista = ar.id_artista
+INNER JOIN user_artista AS ua
+ON ua.id_artista = a.id_artista
+GROUP BY ar.nome, a.nome
+ORDER BY seguidores DESC, artista, album
+;
